139adefbe47ddf653315bd338d98801869fdfa6c
SOUMYA KANTA DAS <36956190+dassoumya654@users.noreply.github.com>
2019-03-29 16:20:49 +0530

    Update chl_content_map.R

--- chl_content_map.R
+++ chl_content_map.R
@@ -5,24 +5,20 @@ library(rgdal)
 
 f <- list.files(path = getwd(), pattern = "jp2$",full.names = FALSE)
 
+# Image for Finding chlorophyll content
 Sent_stack <- stack(f, quick= TRUE)
 Sent_stack <- brick(file.choose())
-
-lai <- raster(file.choose())
-
 band5 <- raster(file.choose())
 band4 <- raster(file.choose())
 
-LAI <- 8.452 * ((Sent_stack$sent_stack_2.2- Sent_stack$sent_stack_2.1)/(Sent_stack$sent_stack_2.2 + Sent_stack$sent_stack_2.1))
-
+### Calculate Leaf Area Index ##############################
+LAI <- 0.57 * exp(2.33 * ((Sent_stack$sent_stack_2.2- Sent_stack$sent_stack_2.1)/(Sent_stack$sent_stack_2.2 + Sent_stack$sent_stack_2.1)))
 writeRaster(LAI, "lai_2.tif", format= "GTiff", overwrite = TRUE)
 
-clip1 <- crop(Sent_stack, rice_shp, snap= "in")
-
+### Input the rice cultivated area shapefile ###############
 rice_shp <- readOGR(file.choose())
-
+clip1 <- crop(Sent_stack, rice_shp, snap= "in")
 ref_values <- extract(Sent_stack, rice_shp, df=TRUE )
-
 names(ref_values) <- paste0("B",c(4:7))
 names(ref_values)[1] <- "ID"
 
@@ -30,19 +26,17 @@ sub_wave <- ref_values[2:5]
 names(sub_wave) <- paste0('B',4:7)
 
 d <- as.data.frame(t(sub_wave))
-
 l <- c(665,703.9,740.2,782.5)
 
 plot(l,d[,1])
 lines(l~d[,1],type="o", lwd=2)
-
 for (i in 1:1000) {
   points(l,d[,i])
 }
 
+### fit a Linear Model ######################################
 c <- lm(l~d[,j])
 b <- predict.lm(c)
-
 
 out <- as.data.frame(NULL)
 for (j in 1:2000) {
@@ -52,19 +46,20 @@ for (j in 1:2000) {
   out[j, 3] <- m$r.squared
 }
 
-out[which.min(out$V3),]
-
-v <- function(f){0.05942066*f + 525.1035}
-a <- integrate(v ,665 ,783)
+min <- out[which.min(out$V3),]
+##############################################################
+v <- function(f){val_a *f + val_b}
+a <- integrate(v , lamda_1 , lamda_2)
 
 h <- max(sub_wave$B7)
 
+### Find Normalized Area Under Curve
 naoc <- 1 - ((a$value)/(h *(783 - 665)))
-
 o <- ((0.0219) * (exp(10.02 * naoc)))
 
+### Input the LAI #############################################
 r <- raster(file.choose())
 
+### Chlorophyll content map ###################################
 chloro <- o/r
-
 writeRaster(chloro, "cholorphyll.tif", format= "GTiff", overwrite = TRUE)
